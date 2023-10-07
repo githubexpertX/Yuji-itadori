8c44c2769832f950176b3766c461f5ace608824d
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-07 16:50:25 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -31,3 +31,44 @@ bot_token = '6561918588:AAE74I_F1bpCi6SltNQHTYClgpM_AThQmNk'
 # Create a Pyrogram client instance
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
+
+@app.on_message(
+    filters.command('start') | filters.command('start@KomiSanRobot'))
+def start(_, message):
+    try:
+        if message.chat.type == "private":
+            users = col.find({})
+            mfs = []
+            for x in users:
+                mfs.append(x['user_id'])
+            if message.from_user.id not in mfs:
+                user = {"type": "user", "user_id": message.from_user.id}
+                col.insert_one(user)
+
+        else:
+            users = grps.find({})
+            mfs = []
+            for x in users:
+                mfs.append(x['chat_id'])
+            if message.chat.id not in mfs:
+                grp = {"type": "group", "chat_id": message.chat.id}
+                grps.insert_one(grp)
+
+    except Exception as e:
+        app.send_message(-1001954317013, f"error in adding stats:\n\n{e}")
+
+    if message.chat.type == "private" and not "help" in message.text:
+
+        app.send_message(
+            message.chat.id,
+            f"ʜᴇʟʟᴏ {message.from_user.mention}\n\nʏᴜᴊɪ ɪᴛᴀᴅᴏʀɪ ʜᴇʀᴇ\nɪᴍ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ\nɪ ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ \n\nɪғ ʏᴏᴜ ɴᴏᴛɪᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴡɪᴛʜ ᴛʜᴇ ʙᴏᴛ, ʏᴏᴜ ᴄᴀɴ ʀᴇᴘᴏʀᴛ ɪᴛ ɪɴ ᴍʏ [sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ](https://t.me/NiGhT_StARs_ll)",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton('help', callback_data="help")]]))
+    if "help" in message.text:
+        app.send_message(message.chat.id,
+                         "Help",
+                         reply_markup=InlineKeyboardMarkup([[
+                             InlineKeyboardButton('help', callback_data="help")
+                         ]]))
+    if not message.chat.type == "private":
+        message.reply("ʜᴇʟʟᴏ ᴛʜᴇʀᴇ ɪᴍ ʏᴜᴊɪ ɪᴛᴀᴅᴏʀɪ")
