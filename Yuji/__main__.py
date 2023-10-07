6bf1265d84d8fbd59a7ba3fd3d635e453f7f779e
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-07 19:17:27 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -26,6 +26,7 @@ import os
 import asyncio
 from pyrogram import filters
 from pyrogram.types import Message
+from requests import post, get
 from pymongo import MongoClient
 from Yuji.db import MONGO_URL as db_url
 from typing import List, Any
@@ -209,6 +210,26 @@ async def run(client, message):
 @app.on_message(filters.command('eye'))
 async def eye(client, message):
     await message.reply_text(choice(EYES))
+
+
+def paste(text):
+    url = "https://spaceb.in/api/v1/documents/"
+    res = post(url, data={"content": text, "extension": "txt"})
+    return f"https://spaceb.in/{res.json()['payload']['id']}"
+
+
+@app.on_message(filters.command('paste'))
+def pastex(_, message):
+    text = message.reply_to_message
+    if text:
+        x = paste(text.text)
+        message.reply(x,
+                      reply_markup=InlineKeyboardMarkup(
+                          [[InlineKeyboardButton("Open", url=x)]]),
+                      disable_web_page_preview=True)
+
+    else:
+        message.reply_text("Reply to a message!")
 
 
 @app.on_message(filters.command('id'))
