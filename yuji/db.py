5f097d45f233d23c202ac0481c403ade47f86745
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-07 16:04:17 +0530

    Delete yuji directory

--- yuji/db.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-import os
-
-MONGO_URL = os.environ.get('mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority')
-
-database = pymongo.MongoClient(MONGO_URL)['notes']['notes']
