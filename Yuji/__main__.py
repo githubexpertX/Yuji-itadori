e48021255e3b0ac4505f0613416731c7517d2ed5
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-07 16:28:16 +0530

    Create __main__.py

---
+++ Yuji/__main__.py
@@ -0,0 +1,19 @@
+from Yuji import app
+import logging
+
+logging.basicConfig(
+    filename="log.txt",
+    level=logging.INFO,
+    filemode="a",
+    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+
+def main():
+    app.run()
+    app.send_message(-1001954317013, "I'm Now online")
+
+
+if __name__ == "__main__":
+    main()
