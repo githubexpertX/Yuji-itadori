13b40ac67bee9f7a82844e6990818f1f7f4076a6
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-07 23:03:21 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -66,6 +66,7 @@ bot_token = '6561918588:AAE74I_F1bpCi6SltNQHTYClgpM_AThQmNk'
 
 # Create a Pyrogram client instance
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = TelegramClient("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://te.legra.ph/file/403acffa5ca195e17112b.jpg')
@@ -222,6 +223,88 @@ async def run(client, message):
 @app.on_message(filters.command('eye'))
 async def eye(client, message):
     await message.reply_text(choice(EYES))
+
+BUTTON = [[Button.url("🍒 ꜱᴜᴘᴘᴏʀᴛ 🍒", "https://t.me/fuck_uff_XD")]]
+HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
+SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
+LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
+BIGBALL = "https://i.gifer.com/8ZUg.gif"
+LANG = "https://telegra.ph/file/423414459345bf18310f5.gif"
+CUTIE = "https://64.media.tumblr.com/d701f53eb5681e87a957a547980371d2/tumblr_nbjmdrQyje1qa94xto1_500.gif"
+SIGMA = "https://te.legra.ph/file/7ff836ce726f9efce590c.mp4"
+
+
+@app.on(events.NewMessage(pattern="/horny ?(.*)"))
+async def horny(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)}]"
+    mm = random.randint(1, 100)
+    HORNY = f"**🔥** {mention} **ɪꜱ** {mm}**% ʜᴏʀɴʏ!**"
+    await e.reply(HORNY, buttons=BUTTON, file=HOT)
+
+
+@app.on(events.NewMessage(pattern="/gay ?(.*)"))
+async def gay(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    GAY = f"**🍷** {mention} **ɪꜱ** {mm}**% ɢᴀʏ!**"
+    await e.reply(GAY, buttons=BUTTON, file=SMEXY)
+
+
+@app.on(events.NewMessage(pattern="/lezbian ?(.*)"))
+async def lezbian(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    FEK = f"**💜** {mention} **ɪꜱ** {mm}**% ʟᴇᴢʙɪᴀɴ!**"
+    await e.reply(FEK, buttons=BUTTON, file=LEZBIAN)
+
+
+@app.on(events.NewMessage(pattern="/boob ?(.*)"))
+async def boob(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    BOOBS = f"**🍒** {mention}**'ꜱ ʙᴏᴏʙꜱ ꜱɪᴢᴇ ɪᴢ** {mm}**!**"
+    await e.reply(BOOBS, buttons=BUTTON, file=BIGBALL)
+
+
+@app.on(events.NewMessage(pattern="/cock ?(.*)"))
+async def cock(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    COCK = f"**🍆** {mention}**'ꜱ ᴄᴏᴄᴋ ꜱɪᴢᴇ ɪᴢ** {mm}**ᴄᴍ**"
+    await e.reply(COCK, buttons=BUTTON, file=LANG)
+
+
+@app.on(events.NewMessage(pattern="/cute ?(.*)"))
+async def cute(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    CUTE = f"**🍑** {mention} {mm}**% ᴄᴜᴛᴇ**"
+    await e.reply(CUTE, buttons=BUTTON, file=CUTIE)
+
+
+@app.on(events.NewMessage(pattern="/chad ?(.*)"))
+async def chad(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    CHAD = f"**🔥** {mention} **ɪꜱ** {mm}**% ᴄʜᴀᴅ!**"
+    await e.reply(CHAD, buttons=BUTTON, file=SIGMA)
+
+
+
 
 
 # SHIVA MISHRA
