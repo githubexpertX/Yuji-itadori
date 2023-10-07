75d86c2b5393419345a91606ea62b98c83ab3404
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-07 23:23:25 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -458,41 +458,56 @@ help_message.append({"Module_Name": "telegraph"})
 
 help_message.append({"Module_Name": "alive"})
 
+help_message.append({"Module_Name": "sexsy"})
+
 
 class Help_Text:
     helpp = {
           "admin_help": """
-• /ban - __reply to a user__
-• /unban __user id or username__
-• /pin - __reply to a message__
-• /unpin - __reply to a message__
-• /id - __to get the user id__
-• /purge - __purge the messages__
+➻ /ban - __reply to a user__
+➻ /unban __user id or username__
+➻ /pin - __reply to a message__
+➻ /unpin - __reply to a message__
+➻ /id - __to get the user id__
+➻ /purge - __purge the messages__
 
 """,
+          "sexsy_help": """
+➻ /horny - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ʜᴏʀɴʏᴇꜱꜱ
+
+➻ /gay - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ɢᴜʏɴᴇꜱꜱ
+
+➻ /lezbian - ᴄʜᴇᴄᴋ ᴜʀ ᴄᴜʀʀᴇɴᴛ ʟᴀᴢʙɪᴀɴ
+
+➻ /boob - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ʙᴏᴏʙꜱ ꜱɪᴢᴇ
+
+➻ /cute - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴄᴜᴛᴇɴᴇꜱꜱ
+
+➻ /chad - ᴄʜᴇᴄᴋ ʜᴏᴡ ᴄʜᴀᴅ ʏᴏᴜ ᴀʀᴇ
+"""
           "meme_help": """
-• /rmeme - __to get random anime memes__
+➻ /rmeme - __to get random anime memes__
 """,
           "alive_help": """
-• /alive - __to check bot alive or not__
+➻ /alive - __to check bot alive or not__
 """,
           "notes_help": """
-• /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__
+➻ /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__
 """,
           "extra_help": """• /watchorder __anime name - to get watchorder__
-• /quote - __to get random anime quotes__
-• /pat - __ to pat someone__
-• /invitelink - __get invitelink of the current chat__
-• /whatanime - __reply to a gif or video__
-• /wall your query - __wallpapers__ 
-• /paste - __reply to a message__
-• define word - __get meaning of a word__
+➻ /quote - __to get random anime quotes__
+➻ /pat - __ to pat someone__
+➻ /invitelink - __get invitelink of the current chat__
+➻ /whatanime - __reply to a gif or video__
+➻ /wall your query - __wallpapers__ 
+➻ /paste - __reply to a message__
+➻ define word - __get meaning of a word__
 
 """,
           "welcome_help": """• /setwelcome __welcome message - text for welcome__
-• /clearwelcome - __clear welcome message__""",
+➻ /clearwelcome - __clear welcome message__""",
         "fun_help": """•/eye
-•/run""",
+➻ /run""",
         "telegraph_help": """• /ul - __Reply to a photo or video__""",
     }
 
