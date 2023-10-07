0ba1f709a288cd42aedd797a3af61b5eb4c9f7c4
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-07 21:40:30 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -29,6 +29,8 @@ from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeybo
 from pyrogram import filters
 import os
 import asyncio
+from typing import Dict, Union
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from pyrogram import filters
 from pyrogram.types import Message
 from requests import post, get
@@ -49,6 +51,11 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
+import random
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.enums import ChatType
+
 
 
 
@@ -74,6 +81,122 @@ help_message = []
 
 OWNER = 65299686966529968696
 
+mongo = MongoCli(db_url)
+db = mongo.app
+
+coupledb = db.couple
+karmadb = db.karma
+
+
+async def _get_lovers(chat_id: int):
+    lovers = await coupledb.find_one({"chat_id": chat_id})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"couple": lovers}},
+        upsert=True,
+    )
+
+
+async def get_karmas_count() -> dict: 
+     chats_count = 0 
+     karmas_count = 0 
+     async for chat in karmadb.find({"chat_id": {"$lt": 0}}): 
+         for i in chat["karma"]: 
+             karma_ = chat["karma"][i]["karma"] 
+             if karma_ > 0: 
+                 karmas_count += karma_ 
+         chats_count += 1 
+     return {"chats_count": chats_count, "karmas_count": karmas_count} 
+  
+  
+async def user_global_karma(user_id) -> int: 
+     total_karma = 0 
+     async for chat in karmadb.find({"chat_id": {"$lt": 0}}): 
+         karma = await get_karma(chat["chat_id"], await int_to_alpha(user_id)) 
+         if karma and (int(karma["karma"]) > 0): 
+             total_karma += int(karma["karma"]) 
+     return total_karma 
+  
+  
+async def get_karmas(chat_id: int) -> Dict[str, int]: 
+     karma = await karmadb.find_one({"chat_id": chat_id}) 
+     if not karma: 
+         return {} 
+     return karma["karma"] 
+  
+  
+async def get_karma(chat_id: int, name: str) -> Union[bool, dict]: 
+     name = name.lower().strip() 
+     karmas = await get_karmas(chat_id) 
+     if name in karmas: 
+         return karmas[name] 
+  
+  
+async def update_karma(chat_id: int, name: str, karma: dict): 
+     name = name.lower().strip() 
+     karmas = await get_karmas(chat_id) 
+     karmas[name] = karma 
+     await karmadb.update_one( 
+         {"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True 
+     ) 
+  
+  
+async def is_karma_on(chat_id: int) -> bool: 
+     chat = await karmadb.find_one({"chat_id_toggle": chat_id}) 
+     if not chat: 
+         return True 
+     return False 
+  
+  
+async def karma_on(chat_id: int): 
+     is_karma = await is_karma_on(chat_id) 
+     if is_karma: 
+         return 
+     return await karmadb.delete_one({"chat_id_toggle": chat_id}) 
+  
+  
+async def karma_off(chat_id: int): 
+     is_karma = await is_karma_on(chat_id) 
+     if not is_karma: 
+         return 
+     return await karmadb.insert_one({"chat_id_toggle": chat_id}) 
+  
+  
+async def int_to_alpha(user_id: int) -> str: 
+     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"] 
+     text = "" 
+     user_id = str(user_id) 
+     for i in user_id: 
+         text += alphabet[int(i)] 
+     return text 
+  
+  
+async def alpha_to_int(user_id_alphabet: str) -> int: 
+     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"] 
+     user_id = "" 
+     for i in user_id_alphabet: 
+         index = alphabet.index(i) 
+         user_id += str(index) 
+     user_id = int(user_id) 
+     return user_id
 
       
       
@@ -221,6 +344,74 @@ async def run(client, message):
 @app.on_message(filters.command('eye'))
 async def eye(client, message):
     await message.reply_text(choice(EYES))
+
+
+# Date and time
+def dt():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    dt_list = dt_string.split(" ")
+    return dt_list
+
+
+def dt_tom():
+    a = (
+        str(int(dt()[0].split("/")[0]) + 1)
+        + "/"
+        + dt()[0].split("/")[1]
+        + "/"
+        + dt()[0].split("/")[2]
+    )
+    return a
+
+
+today = str(dt()[0])
+tomorrow = str(dt_tom())
+
+
+@app.on_message(filters.command(["couple", "couples"]))
+async def couple(_, message):
+    if message.chat.type == ChatType.PRIVATE:
+        return await message.reply_text("This command only works in groups.")
+    try:
+        chat_id = message.chat.id
+        is_selected = await get_couple(chat_id, today)
+        if not is_selected:
+            list_of_users = []
+            async for i in pbot.get_chat_members(message.chat.id, limit=50):
+                if not i.user.is_bot:
+                    list_of_users.append(i.user.id)
+            if len(list_of_users) < 2:
+                return await message.reply_text("Not enough users")
+            c1_id = random.choice(list_of_users)
+            c2_id = random.choice(list_of_users)
+            while c1_id == c2_id:
+                c1_id = random.choice(list_of_users)
+            c1_mention = (await pbot.get_users(c1_id)).mention
+            c2_mention = (await pbot.get_users(c2_id)).mention
+
+            couple_selection_message = f"""**Couple of the day :**
+
+{c1_mention} + {c2_mention} = 😘
+__New couple of the day can be chosen at 12AM {tomorrow}__"""
+            await app.send_message(message.chat.id, text=couple_selection_message)
+            couple = {"c1_id": c1_id, "c2_id": c2_id}
+            await save_couple(chat_id, today, couple)
+
+        elif is_selected:
+            c1_id = int(is_selected["c1_id"])
+            c2_id = int(is_selected["c2_id"])
+            c1_name = (await pbot.get_users(c1_id)).mention
+            c2_name = (await pbot.get_users(c2_id)).mention
+            couple_selection_message = f"""Couple of the day :
+
+{c1_name} + {c2_name} = 😘
+__New couple of the day can be chosen at 12AM {tomorrow}__"""
+            await app.send_message(message.chat.id, text=couple_selection_message)
+    except Exception as e:
+        print(e)
+        await message.reply_text(e)
+
 
 @app.on_message(filters.command("alive"))
 async def awake(_, message: Message):
