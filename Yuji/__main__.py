0ee5ddbf8e4311c31e95384193a25cd80e2b1584
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-08 19:22:28 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -994,6 +994,8 @@ help_message.append({"Module_Name": "pin"})
 
 help_message.append({"Module_Name": "title"})
 
+help_message.append({"Module_Name": "song"})
+
 class Help_Text:
     helpp = {
           "admin_help": """
@@ -1029,6 +1031,10 @@ class Help_Text:
 """,
           "notes_help": """
 ➻ /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__
+""",
+          "song_help": """ 
+/song ᴛᴏ  ᴅᴏᴡɴʟᴏᴀᴅ   ᴀɴʏ  sᴏɴɢ  
+/music ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴀɴʏ  sᴏɴɢ
 """,
           "shayri_help":"""ꜱᴇɴᴅ ʀᴀɴᴅᴏᴍ ꜱʜᴀʏʀɪ
 ❍ /loveshayri : ʟᴏᴠᴇ ꜱʜᴀʏʀɪ
