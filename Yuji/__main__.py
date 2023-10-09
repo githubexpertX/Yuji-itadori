aae03b3097c4d891d469a4577acfc381513eebf6
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-09 09:42:00 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -178,7 +178,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/2a457256aed0ca9a4fcd5.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/16c5ea4390fcb09ac5a20.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", 'AQG2aTkAS47gsetpjbftb2GfcPSr-Si_hRtz_-8KWCaLzQrMsrh4Y71PvFB2c-BpI35Fnno1ZeLANaAmXxgyV4x1QTaCoo-BW4EO405VAXojWJoNKmLGeGUwSanAkm-4i55yXP74XzV3t330tVtuuXqx4m7Btsf5DWhW9SNBFiyjX5LZesYFCo0vAiKOK7YT9K_qReTBRHtAF27t92M43b8r6Yn38WKSp4a_VkUQwyewKlCE4DLTjrADA3wXJrvgDezjcXJij3u3rwK8ReVknyjKMeGPFtSnSfAyKOLD40KQxKLhjk-hISNVYpUTlykgO5pmu8U3T6r1LVsXXcakcBZxZ9pTzAAAAAGFN2o4AA')
 
 SUPPORT_GRP = getenv("SUPPORT_CHAT", "https://t.me/fuck_uff_XD")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/fuck_uff_XD")
