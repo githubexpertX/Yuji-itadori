396fd97a06aec9e7cf2c710ee2cd5e1ac8a5732c
RARERABBIT <131595098+purshottam678@users.noreply.github.com>
2023-10-09 12:43:31 +0530

    Update __main__.py

--- Yuji/__main__.py
+++ Yuji/__main__.py
@@ -144,8 +144,24 @@ from telethon import __version__ as tlhver
 import random
 from datetime import datetime
 from pyrogram import filters
-
-
+import asyncio
+import importlib
+import os
+from pyrogram import idle
+from Yuji import (
+    ASS_ID,
+    ASS_NAME,
+    ASS_USERNAME,
+    BOT_ID,
+    BOT_NAME,
+    BOT_USERNAME,
+    LOGGER,
+    SUNAME,
+    app3,
+    app2,
+    pytgcalls,
+)
+from Yuji.Modules import ALL_MODULES
 
 
 # Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your actual API credentials
@@ -1343,3 +1359,53 @@ print(f"⚡  Bot ⚡ [INFO]:  Pyrogram Version - 6.8.9")
 print("➖➖➖➖➖➖➖➖➖➖➖➖")
 
 app.run()
+
+
+async def fallen_startup():
+    LOGGER.info("[•] Loading Modules...")
+    for module in ALL_MODULES:
+        importlib.import_module("FallenMusic.Modules." + module)
+    LOGGER.info(f"[•] Loaded {len(ALL_MODULES)} Modules.")
+
+    LOGGER.info("[•] Refreshing Directories...")
+    if "downloads" not in os.listdir():
+        os.mkdir("downloads")
+    if "cache" not in os.listdir():
+        os.mkdir("cache")
+    LOGGER.info("[•] Directories Refreshed.")
+
+    try:
+        await app3.send_message(
+            SUNAME,
+            f" ᴍᴜsɪᴄ  \n\n ɪᴅ : `{BOT_ID}`\n ɴᴀᴍᴇ : {BOT_NAME}\n ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
+        )
+    except:
+        LOGGER.error(
+            f"{BOT_NAME} failed to send message at @{SUNAME}, please go & check."
+        )
+
+    try:
+        await app2.send_message(
+            SUNAME,
+            f" ᴍᴜsɪᴄ  \n\n ɪᴅ : `{ASS_ID}`\n ɴᴀᴍᴇ : {ASS_NAME}\n ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
+        )
+    except:
+        LOGGER.error(
+            f"{ASS_NAME} failed to send message at @{SUNAME}, please go & check."
+        )
+
+    await app2.send_message(BOT_USERNAME, "/start")
+
+    LOGGER.info(f"[•] Bot Started As {BOT_NAME}.")
+    LOGGER.info(f"[•] Assistant Started As {ASS_NAME}.")
+
+    LOGGER.info(
+        "[•] \x53\x74\x61\x72\x74\x69\x6e\x67\x20\x50\x79\x54\x67\x43\x61\x6c\x6c\x73\x20\x43\x6c\x69\x65\x6e\x74\x2e\x2e\x2e"
+    )
+    await pytgcalls.start()
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(fallen_startup())
+    LOGGER.error(" Music Bot Stopped.")
